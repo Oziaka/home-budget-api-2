10eaaf740ca3283638a9fb0aafedc367ec5ef08f
Oziaka <daniel.madzierski.pl@gmail.com>
2020-07-11 16:39:37 +0200

    Rebuild database,added Notification, item to user and notification

---
+++ src/main/java/pl/user/item_key/UserItemKey.java
@@ -0,0 +1,26 @@
+package pl.user.item_key;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class UserItemKey {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_item_key_id")
+  private Long id;
+
+  private String name;
+
+  @Builder
+  public UserItemKey (String name) {
+    this.name = name;
+  }
+}
